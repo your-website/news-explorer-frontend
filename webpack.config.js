81891f10f0e30ab42947298632ae5cdcba38392b
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-02-28 00:37:48 +0700

    add preloader

--- webpack.config.js
+++ webpack.config.js
@@ -179,4 +179,90 @@ const login = {
   ]
 };
 
-module.exports = [main, login];
+const news = {
+  entry: { main: './src/saved-news.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: { loader: "babel-loader" },
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.(woff|woff2|ttf|otf)$/i,
+      loader: 'file-loader?name=./vendor/[name].[ext]'
+    },
+    {
+      test: /\.(png|jpe?g|gif|svg|ico)$/i,
+      use: [
+        'file-loader?name=./images/[name].[ext]',
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              progressive: true,
+              quality: 90
+            },
+            optipng: {
+              enabled: false,
+            },
+            pngquant: {
+              quality: [0.75, 0.90],
+              speed: 4
+            },
+            gifsicle: {
+              interlaced: false,
+            },
+            webp: {
+              quality: 90
+            }
+          }
+        },
+      ],
+    },
+    {
+      test: /\.css$/,
+      use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader']
+    },
+    {
+      test: /\.scss$/i,
+      use: [
+          (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+      ]
+    }
+    ]
+  },
+  devServer: {
+    overlay: true
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/saved-news.html',
+      filename: 'saved-news.html'
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true
+    }),
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+};
+
+module.exports = [main, login, news];
