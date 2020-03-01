41b06e69191a8469de5c132ffe9c47158eb4b756
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-03-01 16:57:42 +0700

    fix dependencies. add validate for for-authorization

--- src/js/modules/form-authorization/form-authorization.js
+++ src/js/modules/form-authorization/form-authorization.js
@@ -8,7 +8,7 @@ const authorization__contentTitle = document.querySelector('.authorization__cont
 const authorization__paragraph = document.querySelector('.authorization__paragraph');
 const authorization__paragraph_success = document.querySelector('.authorization__paragraph_success');
 const form = document.forms.authorization;
-
+const submit = form.elements.submit;
 const name = form.elements.name;
 const password = form.elements.password;
 const email = form.elements.email;
@@ -17,12 +17,34 @@ const formAuthorization__error_email = document.querySelector('.form-authorizati
 const formAuthorization__error_passwrod = document.querySelector('.form-authorization__error_password');
 const validator = require('validator');
 
+function submitDisabled() {
+    if (authorization__container_registration.style.display === 'flex') {
+        submitDisabledPlusName();
+        return;
+    }
+    if (!validateEmail() || !validatePassword()) {
+        formAuthorization__submit.classList.add('form-authorization__submit_disabled');
+    } else {
+        formAuthorization__submit.classList.remove('form-authorization__submit_disabled');
+    }
+}
+
+function submitDisabledPlusName() {
+    if (!validateEmail() || !validatePassword() || !validateName()) {
+        formAuthorization__submit.classList.add('form-authorization__submit_disabled');
+    } else {
+        formAuthorization__submit.classList.remove('form-authorization__submit_disabled');
+    }
+}
+
 function validateEmail() {
     if (!validator.isEmail(email.value)) {
         formAuthorization__error_email.style.display = 'block';
+        submit.setAttribute('disabled', true);
         return false;
     } else {
         formAuthorization__error_email.style.display = 'none';
+        submit.removeAttribute ('disabled');
         return true;
     }
 }
@@ -30,9 +52,11 @@ function validateEmail() {
 function validatePassword() {
     if (!validator.isLength(password.value, {min:2, max: undefined})) {
         formAuthorization__error_passwrod.style.display = 'block';
+        submit.setAttribute('disabled', true);
         return false;
     } else {
         formAuthorization__error_passwrod.style.display = 'none';
+        submit.removeAttribute ('disabled');
         return true;
     }
 }
@@ -40,9 +64,11 @@ function validatePassword() {
 function validateName() {
     if (validator.isEmpty(name.value)) {
         formAuthorization__error_name.style.display = 'block';
+        submit.setAttribute('disabled', true);
         return false;
     } else {
         formAuthorization__error_name.style.display = 'none';
+        submit.removeAttribute ('disabled');
         return true;
     }
 }
@@ -54,18 +80,6 @@ function success() {
     authorization__paragraph.style.display = 'none';
 };
 
-
-form.addEventListener('submit', function () {
-    event.preventDefault();
-    if (authorization__container_registration.style.display === 'flex') {
-        if (validateEmail() && validatePassword() && validateName()) {
-            success();
-        } else return
-    } else if (validateEmail() && validatePassword()) {
-        success();
-    } else return;
-});
-
 function openPopUp() {
     form.style.display = 'flex';
     authorization.style.display = 'flex';
@@ -75,6 +89,8 @@ function openPopUp() {
     authorization__contentTitle.textContent = 'Войти';
     authorization__paragraph_success.style.display = 'none';
     authorization__paragraph.style.display = 'block';
+    submit.setAttribute('disabled', true);
+    password.value = '';
 }
 
 function closePopUp() {
@@ -82,22 +98,36 @@ function closePopUp() {
 }
 
 function changePopUp() {
-
+    password.value = '';
+    submit.setAttribute('disabled', true);
     if (authorization__span.textContent === 'Зарегистрироваться') {
         authorization__container_registration.style.display = 'flex';
         authorization__span.textContent = 'Войти';
-        formAuthorization__submit.classList.remove('form-authorization__submit_disabled');
         authorization__contentTitle.textContent = 'Регистрация';
     } else {
         authorization__container_registration.style.display = 'none';
         authorization__span.textContent = 'Зарегистрироваться';
-        formAuthorization__submit.classList.add('form-authorization__submit_disabled');
         authorization__contentTitle.textContent = 'Войти';
     }
-    
 }
+
+document.addEventListener('click', function(event) {
+    if (event.target.classList.value === 'authorization') {
+        closePopUp();
+    }
+});
 
 authorization__svg.addEventListener('click', closePopUp);
 header__button.addEventListener('click', openPopUp);
 authorization__span.addEventListener('click', changePopUp);
 authorization__paragraph_success.addEventListener('click', openPopUp);
+
+
+email.addEventListener('input', submitDisabled);
+password.addEventListener('input', submitDisabled);
+name.addEventListener('input', submitDisabledPlusName);
+
+form.addEventListener('submit', function () {
+    event.preventDefault();
+    success();
+});
